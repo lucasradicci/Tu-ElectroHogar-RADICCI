c0e567e72322d8145317d775e1c6808e161ab209
lucas radicci <radicci.lukas@gmail.com>
2024-09-04 17:52:58 -0300

    realice cambios en el array de productos, en la variable let del carrito y aplique una funcion para mostrar los productos

--- main.js
+++ main.js
@@ -5,7 +5,22 @@ const productos = [
     { id: 3, nombre: 'Microondas', precio: 150 },
     { id: 4, nombre: 'Televisor', precio: 1000 },
     { id: 5, nombre: 'Horno Eléctrico', precio: 250},
-    { id: 4, nombre: 'Televisor', precio: 1000 }
+    
 ];
 
-let carrito= JSON.parse(localStorage.getItem('carrito'))
+let carrito= JSON.parse(localStorage.getItem('carrito')) ||[];
+
+function mostrarProductos(){
+    const listaProductos= document.getElementById('listaProductos');
+    listaProductos.innerHTML='';
+
+    productos.forEach(producto=>{
+        const li= document.createElement('li');
+        li.textContent= `${producto.nombre} - $${producto.precio}`;
+        const button= document.createElement('button');
+        button.textContent='Agregar al Carrito';
+        button.addEventListener('click', ()=> agregarAlCarrito(producto.id));
+        li.appendChild(li); 
+    })
+}
+mostrarProductos()
