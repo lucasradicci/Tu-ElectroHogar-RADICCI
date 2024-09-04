a5e07ba712e6658308a6a3710f6a4d41113c7077
lucas radicci <radicci.lukas@gmail.com>
2024-09-04 16:44:56 -0300

    cree un array de los productos de mi tienda

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const productos = [
+    { id: 1, nombre: 'Heladera', precio: 800 },
+    { id: 2, nombre: 'Lavarropas', precio: 500 },
+    { id: 3, nombre: 'Microondas', precio: 150 },
+    { id: 4, nombre: 'Televisor', precio: 1000 },
+    { id: 5, nombre: 'Horno Eléctrico', precio: 250},
+    { id: 4, nombre: 'Televisor', precio: 1000 }
+];
+
+
